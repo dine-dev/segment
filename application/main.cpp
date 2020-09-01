66255238248f07451f1c5b7d1a2b073e90b7abdc
bdz <bdz@mac-1.home>
2020-09-01 17:55:25 +0200

    update metadata

--- application/main.cpp
+++ application/main.cpp
@@ -2,21 +2,16 @@
 
 int main(int argc, char **argv)
 {
-    std::cout << pagesWithRoubou3.size() << std::endl;
-    int nAyah = 0;
-    for (size_t it = 1; it < 605; it++)
-    //for (std::map<int,int>::const_iterator it = pagesWithSourateHeader.begin(); it != pagesWithSourateHeader.end(); ++it)
-    //for (std::map<int,int>::const_iterator it = pagesWithBasmala.begin(); it != pagesWithBasmala.end(); ++it)
-    // for (std::map<int,int>::const_iterator it = pagesWithSoujoud.begin(); it != pagesWithSoujoud.end(); ++it)
-    // for (std::map<int, int>::const_iterator it = pagesWithRoubou3.begin(); it != pagesWithRoubou3.end(); ++it)
+    // for (size_t it = 1; it < 605; it++)
+    for (std::map<int, int>::const_iterator it = pagesAyahNumber.begin(); it != pagesAyahNumber.end(); ++it)
     {
-        QuranImagePage qimage(it);
-        //qimage.showContours();
-        std::vector<cv::Rect> ayah_panel;
-        qimage.hasAyah(ayah_panel);
-        nAyah += ayah_panel.size();
+        // QuranImagePage qimage(it->first);
+        QuranImagePage qimage(it->first);
+        qimage.showContours();
+        // std::vector<cv::Rect> ayah_panel;
+        // qimage.hasAyah(ayah_panel);
+        // std::cout << "{" << it->first << "," << ayah_panel.size() << "},";
     }
-        std::cout << "number of ayah " << nAyah << std::endl;
 
     return 0;
 }
